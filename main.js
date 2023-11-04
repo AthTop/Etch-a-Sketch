78164633d7420e2f231968a4142d127be72a76d6
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-11-04 12:37:18 +0200

    Prompt for grid size. Reset board

--- main.js
+++ main.js
@@ -7,17 +7,43 @@ function createDiv(className) {
 const container = createDiv('flex-container')
 document.body.appendChild(container)
 
-for (let i = 0; i < 16; i++) {
-    container.appendChild(createDiv("flex-row"))
-    for (let j = 0; j < 16; j++) {
-        container.lastChild.appendChild(createDiv("cell"))
+function clearContainer(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
 }
 
-const cells = document.querySelectorAll('.cell')
-
-cells.forEach(cell => {
-    cell.addEventListener("mouseenter", (e) => {
-        e.target.style.background = 'blue';
-    });
+function generateGrid(size){
+    clearContainer()
+    for (let i = 0; i < size; i++) {
+        container.appendChild(createDiv("flex-row"))
+        for (let j = 0; j < size; j++) {
+            container.lastChild.appendChild(createDiv("cell"))
+        }
+    }
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.addEventListener("mouseenter", (e) => {
+            e.target.style.background = 'red';
+        });
 });
+}
+
+const resetButton = document.getElementById('reset')
+resetButton.addEventListener('click', resetBoard);
+
+function resetBoard(){
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.style.background = '#eeeeee'
+    })
+}
+
+const newGridButton = document.getElementById('new')
+
+newGridButton.addEventListener('click', () => {
+    let size = prompt("Enter grid size")
+    generateGrid(size)
+});
+
+
